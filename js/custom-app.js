3fc683cafb967374fccf4386c9412f59da9ca683
ALBERTO MIRANDA <allowski@gmail.com>
2015-08-07 09:19:10 -0400

    15-08-07

--- js/custom-app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("deviceready", onDeviceReady, false);
-
-    // device APIs are available
-    //
-    function onDeviceReady() {
-         var ref = window.open('geo:-23.4347092,-58.4483503', '_blank', 'location=yes');
-    }
