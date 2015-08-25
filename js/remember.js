0ca0102cd48ef265dead7110ebff3a2fae9d4d52
root <root@sites2.(none)>
2015-08-25 15:16:59 -0400

    15-08-25

--- js/remember.js
+++ js/remember.js
@@ -12,7 +12,7 @@ function inCollection(obj, list) {
 var remember = {
 	'collections' : {},
 	'debug'	: true,
-	'logs': "init\n"
+	'logs': "init\n",
 	'load' 	: function(){
 		try{
 			this.collections = JSON.parse(window.localStorage['rememberData']);
